e5ae6cd7da8c133a79920c9fbb750e5826a9feac
Yanzz231 <mrpartys05@gmail.com>
2026-03-19 19:18:08 +0700

    feat: register poll routes and socket.io server

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
 import express from 'express'
+import { createServer } from 'http'
 import cors from 'cors'
 import swaggerUi from 'swagger-ui-express'
 import { toNodeHandler } from 'better-auth/node'
 import { auth } from './config/auth.js'
 import { corsOptions } from './config/cors.js'
 import { swaggerSpec } from './config/swagger.js'
+import { initSocket } from './config/socket.js'
 import eventRoutes from './routes/events.js'
 import sectionRoutes from './routes/sections.js'
 import responseRoutes from './routes/responses.js'
 import publicRoutes from './routes/public.js'
+import pollRoutes from './routes/polls.js'
+import pollSlideRoutes from './routes/poll-slides.js'
+import publicPollRoutes from './routes/public-polls.js'
+import { requestLogger } from './middlewares/logger.js'
 
 const app = express()
+const server = createServer(app)
 const PORT = process.env.PORT ?? 3001
+
+initSocket(server)
 
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(requestLogger)
 
 app.all('/api/auth/{*splat}', toNodeHandler(auth))
 
@@ -25,6 +35,9 @@ app.use('/api/events', eventRoutes)
 app.use('/api/events/:eventId/sections', sectionRoutes)
 app.use('/api/events/:eventId/responses', responseRoutes)
 app.use('/api/public', publicRoutes)
+app.use('/api/polls', pollRoutes)
+app.use('/api/polls/:pollId/slides', pollSlideRoutes)
+app.use('/api/public/polls', publicPollRoutes)
 
 app.get('/health', (_req, res) => {
   res.json({
@@ -35,7 +48,7 @@ app.get('/health', (_req, res) => {
   })
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
   console.log(`Swagger docs at http://localhost:${PORT}/api-docs`)
 })
