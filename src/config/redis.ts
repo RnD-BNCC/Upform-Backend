6c16542e7a93cc4ddd9ae65ee10189808e18d6b1
Yanzz231 <mrpartys05@gmail.com>
2026-04-25 13:08:32 +0700

    feat: configure redis client

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,9 @@
-import IORedis from 'ioredis'
+import { Redis } from 'ioredis'
 
 if (!process.env.REDIS_URL) {
   throw new Error('Missing REDIS_URL env var')
 }
 
-export const redis = new IORedis(process.env.REDIS_URL, {
+export const redis = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
 })
