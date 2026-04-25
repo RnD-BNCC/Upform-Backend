52aafa7ce3baa3bc31132651829eaec20281a787
Yanzz231 <mrpartys05@gmail.com>
2026-04-25 19:28:33 +0700

    fix: use prisma db push instead of migrate deploy

--- prisma/migrations/20260425020000_soft_delete_responses/migration.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "Response" ADD COLUMN "deletedAt" TIMESTAMP(3);
-ALTER TABLE "ResponseProgress" ADD COLUMN "deletedAt" TIMESTAMP(3);
-
-CREATE INDEX "Response_eventId_deletedAt_idx" ON "Response"("eventId", "deletedAt");
-CREATE INDEX "ResponseProgress_eventId_deletedAt_idx" ON "ResponseProgress"("eventId", "deletedAt");
